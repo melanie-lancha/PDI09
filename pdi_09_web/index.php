9dfa3c344f00f2cc66e063a0101b108ee964b109
Augustin-Pierre <augustin-pierre.hostein@ensg.eu>
2025-02-18 22:12:56 +0100

    début de l'API / BDD

--- pdi_09_web/index.php
+++ pdi_09_web/index.php
@@ -1,12 +1,31 @@
 <?php
 require 'flight/Flight.php';
 
-// $link =   pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
-// Flight::set('db', $link);
+$link =   pg_connect("host=localhost port=5432 dbname=PDI09 user=postgres password=postgres");
+Flight::set('db', $link);
 
 Flight::route('/', function() {
     Flight::render('accueil'); 
 });
+
+Flight::route('/api/fichiers', function() {  
+    $db = Flight::get('db');
+    $table = $_GET["t"];
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $reponse = pg_query($db,"SELECT * FROM " .$table." where fid = '". $_GET['id']."'");
+        $resultats = pg_fetch_all($reponse, PGSQL_ASSOC)[0];
+    }
+
+    else{
+        $reponse = pg_query($db, "SELECT * from test");
+        $resultats = pg_fetch_all($reponse, PGSQL_ASSOC);
+    }
+
+    Flight::json($resultats);
+});
+
+
+
 
 Flight::start();
 
