e97eeaac253ef382fe6481bbb719c1ff8ceb265c
melanie-lancha <melanie.lancha@gmail.com>
2025-02-18 20:59:53 +0100

    Update javascript.js

--- pdi_09_web/assets/javascript.js
+++ pdi_09_web/assets/javascript.js
@@ -3,7 +3,9 @@ Vue.createApp({
       return {
         map : L.map('map',{zoomAnimation: false}).setView([42.70089753241175, 3.347071849162483], 10),
         marker : L.marker([42.69193509203, 2.8972979683846556]),
+        layer : null,
         perpignan : false,
+        habitat : false,
       };
     },
 
@@ -17,16 +19,15 @@ Vue.createApp({
              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
 
-        L.tileLayer.wms("https://wxs.ofb.fr/geoserver/habitat/ows?", {
+        this.layer = L.tileLayer.wms("https://wxs.ofb.fr/geoserver/habitat/ows?", {
             service: "WMS",
-            version: "1.1.1",  // Essayez 1.1.1 si 1.3.0 ne fonctionne pas
-            layers: "habitat:bio_atl_bzhnd_maerl_2021_ofb_pol_3857",
+            version: "1.1.1", 
+            layers: "habitat d'intérêt",
             format: "image/png",
             attribution: "Données OFB",
             transparent: true,
-            tiled: true,
-        
-        }).addTo(this.map);
+            tiled: true,        
+        })
         
         
     },
@@ -39,6 +40,16 @@ Vue.createApp({
               this.map.removeLayer(this.marker);
             }
           },
+
+          cocher2(){
+            if(this.habitat){
+                this.layer.addTo(this.map);
+            }else{
+              this.map.removeLayer(this.layer);
+            }
+          },
+
+
     },
 
   }).mount('#app');
